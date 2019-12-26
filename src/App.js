888d462a9ed20ba0399e7c5e694e522ad66319df
Raul Costa Junior <raulcostajunior@gmail.com>
2019-12-26 21:51:32 +0100

    Tweaked the lay-out.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import React from 'react';
 // Important info:
 // https://www.garysnotebook.com/20190314_1 (How to use MaterializeCss JS with React)
 // https://codepen.io/cpett/pen/gmJygO (MaterializeCss scrollable tbody)
+// Use custom css styles for board components
 function App() {
     return (
         <div className="container">
             <div className="row">
-                <div className="col s12 m8">
+                <div className="col s12 xl8 offset-xl2">
                     <div className="card-panel">
                         <h6>Generator Panel</h6>
                     </div>
                 </div>
             </div>
             <div className="row">
-                <div className="col s12 m8">
+                <div className="col s12 xl8 offset-xl2">
                     <div className="card-panel">
                         <table>
                             <tbody>
@@ -46,17 +47,17 @@ function App() {
                                     <td>&nbsp;</td>
                                 </tr>
                             </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td>Status: Empty</td>
+                                </tr>
+                            </tfoot>
                         </table>
-                    </div>
-                </div>
-                <div className="col s12 m4">
-                    <div className="card-panel">
-                        <h6>Status Panel</h6>
                     </div>
                 </div>
             </div>
             <div className="row">
-                <div className="col s12 m8">
+                <div className="col s12 xl8 offset-xl2">
                     <div className="card-panel">
                         <h6>Solutions Panel</h6>
                     </div>
